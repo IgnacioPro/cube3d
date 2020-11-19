b0d627f19e84ffb9694b03c5c50ca01bf86fecf5
Ignacio Horcada bernal <ihorcada@c2r4s3.42madrid.com>
2020-11-19 14:15:47 +0100

    movement working with minimal colision

--- raycaster.c
+++ raycaster.c
@@ -11,17 +11,17 @@
 
 typedef struct s_vars
 {
-	void	*mlx;
-	void	*win;
-	double	moveSpeed;
-	int		key;
-	double	rotSpeed;
-	double	posX;
-	double	posY;
-	double	dirX;
-	double	dirY;
-	double	planeX;
-	double	planeY;
+	void *mlx;
+	void *win;
+	double moveSpeed;
+	int key;
+	double rotSpeed;
+	double posX;
+	double posY;
+	double dirX;
+	double dirY;
+	double planeX;
+	double planeY;
 
 } t_vars;
 
@@ -63,19 +63,20 @@ void draw_line(int x, int drawStart, int drawEnd, int color, t_vars *vars)
 
 int move_player(int keycode, t_vars *vars) //
 {
-	vars->key = keycode;
-	mlx_clear_window(vars->mlx, vars->win);
+	// vars->key = keycode;
+	// mlx_clear_window(vars->mlx, vars->win);
+	printf("Casilla: %d\n", worldMap[(int)(vars->posX + vars->dirX * vars->moveSpeed)][(int)vars->posY]);
 	if (keycode == 126) //up
 	{
-		printf("Arriba\n");
 		if (worldMap[(int)(vars->posX + vars->dirX * vars->moveSpeed)][(int)vars->posY] == 0)
 			vars->posX += vars->dirX * vars->moveSpeed;
 		if (worldMap[(int)vars->posX][(int)(vars->posY + vars->dirY * vars->moveSpeed)] == 0)
 			vars->posY += vars->dirY * vars->moveSpeed;
+		
 	}
+
 	if (keycode == 125) //down
 	{
-		printf("Abajo\n");
 		if (worldMap[(int)(vars->posX - vars->dirX * vars->moveSpeed)][(int)vars->posY] == 0)
 			vars->posX -= vars->dirX * vars->moveSpeed;
 		if (worldMap[(int)vars->posX][(int)(vars->posY - vars->dirY * vars->moveSpeed)] == 0)
@@ -83,7 +84,6 @@ int move_player(int keycode, t_vars *vars) //
 	}
 	if (keycode == 124) //right
 	{
-		printf("Derecha\n");
 		double oldDirX = vars->dirX;
 		vars->dirX = vars->dirX * cos(-vars->rotSpeed) - vars->dirY * sin(-vars->rotSpeed);
 		vars->dirY = oldDirX * sin(-vars->rotSpeed) + vars->dirY * cos(-vars->rotSpeed);
@@ -93,7 +93,6 @@ int move_player(int keycode, t_vars *vars) //
 	}
 	if (keycode == 123) //left
 	{
-		printf("Izquierda\n");
 		double oldDirX = vars->dirX;
 		vars->dirX = vars->dirX * cos(vars->rotSpeed) - vars->dirY * sin(vars->rotSpeed);
 		vars->dirY = oldDirX * sin(vars->rotSpeed) + vars->dirY * cos(vars->rotSpeed);
@@ -103,9 +102,13 @@ int move_player(int keycode, t_vars *vars) //
 	}
 	if (keycode == 53)
 	{
-    	mlx_destroy_window(vars->mlx, vars->win);
+		mlx_destroy_window(vars->mlx, vars->win);
 		exit(0);
 	}
+
+	printf("Pos X: %f\n", vars->posX);
+	printf("Pos Y: %f\n", vars->posY);
+
 	return (0);
 }
 
@@ -120,110 +123,107 @@ int render_frame(t_vars *vars)
 {
 	int x = 0;
 	while (x < screenWidth)
+	{
+		double cameraX = 2 * x / (double)screenWidth - 1;
+		double rayDirX = vars->dirX + vars->planeX * cameraX;
+		double rayDirY = vars->dirY + vars->planeY * cameraX;
+		int mapX = vars->posX;
+		int mapY = vars->posY;
+		double sideDistX;
+		double sideDistY;
+		double deltaDistX = ft_abs(1 / rayDirX);
+		double deltaDistY = ft_abs(1 / rayDirY);
+		double perpWallDist;
+
+		int stepX;
+		int stepY;
+		int hit = 0;
+		int side;
+
+		if (rayDirX < 0)
 		{
-			double cameraX = 2 * x / (double)screenWidth - 1;
-			double rayDirX = vars->dirX + vars->planeX * cameraX;
-			double rayDirY = vars->dirY + vars->planeY * cameraX;
-			int mapX = vars->posX;
-			int mapY = vars->posY;
-			double sideDistX;
-			double sideDistY;
-			double deltaDistX = ft_abs(1 / rayDirX);
-			double deltaDistY = ft_abs(1 / rayDirY);
-			double perpWallDist;
-
-			int stepX;
-			int stepY;
-			int hit = 0;
-			int side;
-
-			if (rayDirX < 0)
-			{
-				stepX = -1;
-				sideDistX = (vars->posX - mapX) * deltaDistX;
-			}
-			else
-			{
-				stepX = 1;
-				sideDistX = (mapX + 1.0 - vars->posX) * deltaDistX;
-			}
-			if (rayDirY < 0)
-			{
-				stepY = -1;
-				sideDistY = (vars->posY - mapY) * deltaDistY;
-			}
-			else
-			{
-				stepY = 1;
-				sideDistY = (mapY + 1.0 - vars->posY) * deltaDistY;
-			}
-			while (hit == 0)
-			{
-				if (sideDistX < sideDistY)
-				{
-					sideDistX += deltaDistX;
-					mapX += stepX;
-					side = 0;
-				}
-				else
-				{
-					sideDistY += deltaDistY;
-					mapY += stepY;
-					side = 1;
-				}
-				if (worldMap[mapX][mapY] > 0)
-					hit = 1;
-			}
-			if (side == 0)
-				perpWallDist = (mapX - vars->posX + (1 - stepX) / 2) / rayDirX;
-			else
-				perpWallDist = (mapY - vars->posY + (1 - stepY) / 2) / rayDirY;
-
-			int lineHeight = (int)(screenHeight / perpWallDist);
-			int drawStart = -lineHeight / 2 + screenHeight / 2;
-			if (drawStart < 0)
-				drawStart = 0;
-			int drawEnd = lineHeight / 2 + screenHeight / 2;
-			if (drawEnd >= screenHeight)
-				drawEnd = screenHeight - 1;
-			int color = 0x000000;
-			if (worldMap[mapX][mapY] == 1)
-				color = 0xD63E1E;
-			else if (worldMap[mapX][mapY] == 2)
-				color = 0x30D61C;
-			else if (worldMap[mapX][mapY] == 3)
-				color = 0x16E8E5;
-			else if (worldMap[mapX][mapY] == 4)
-				color = 0xF958E1;
-			else
-				color = 0x050405;
-			if (side == 1)
-				color /= 2;
-
-			draw_line(x, drawStart, drawEnd, color, vars);
-			++x;
+			stepX = -1;
+			sideDistX = (vars->posX - mapX) * deltaDistX;
 		}
+		else
+		{
+			stepX = 1;
+			sideDistX = (mapX + 1.0 - vars->posX) * deltaDistX;
+		}
+		if (rayDirY < 0)
+		{
+			stepY = -1;
+			sideDistY = (vars->posY - mapY) * deltaDistY;
+		}
+		else
+		{
+			stepY = 1;
+			sideDistY = (mapY + 1.0 - vars->posY) * deltaDistY;
+		}
+		while (hit == 0)
+		{
+			if (sideDistX < sideDistY)
+			{
+				sideDistX += deltaDistX;
+				mapX += stepX;
+				side = 0;
+			}
+			else
+			{
+				sideDistY += deltaDistY;
+				mapY += stepY;
+				side = 1;
+			}
+			if (worldMap[mapX][mapY] > 0)
+				hit = 1;
+		}
+		if (side == 0)
+			perpWallDist = (mapX - vars->posX + (1 - stepX) / 2) / rayDirX;
+		else
+			perpWallDist = (mapY - vars->posY + (1 - stepY) / 2) / rayDirY;
+
+		int lineHeight = (int)(screenHeight / perpWallDist);
+		int drawStart = -lineHeight / 2 + screenHeight / 2;
+		if (drawStart < 0)
+			drawStart = 0;
+		int drawEnd = lineHeight / 2 + screenHeight / 2;
+		if (drawEnd >= screenHeight)
+			drawEnd = screenHeight - 1;
+		int color = 0x000000;
+		if (worldMap[mapX][mapY] == 1)
+			color = 0xD63E1E;
+		else if (worldMap[mapX][mapY] == 2)
+			color = 0x30D61C;
+		else if (worldMap[mapX][mapY] == 3)
+			color = 0x16E8E5;
+		else if (worldMap[mapX][mapY] == 4)
+			color = 0xF958E1;
+		else
+			color = 0x050405;
+		if (side == 1)
+			color /= 2;
+
+		draw_line(x, drawStart, drawEnd, color, vars);
+		++x;
+		mlx_clear_window(vars->mlx, vars->win);
+	}
+	return (0);
 }
 int main()
 {
 	t_vars vars;
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, screenWidth, screenHeight, "Cube");
-	int trex = 1;
-	int x = 0;
-	vars.moveSpeed = 0.1;
-	vars.rotSpeed = 0.1;
+	// int x = 0;
+	vars.moveSpeed = 0.3;
+	vars.rotSpeed = 0.2;
 	vars.posX = 22;
 	vars.posY = 12;
 	vars.dirX = -1;
 	vars.dirY = 0;
 	vars.planeX = 0;
 	vars.planeY = 0.66;
-
-
-
 	mlx_loop_hook(vars.mlx, render_frame, &vars);
-	
 	mlx_hook(vars.win, 2, 1L << 0, move_player, &vars);
 	mlx_loop(vars.mlx);
 }
