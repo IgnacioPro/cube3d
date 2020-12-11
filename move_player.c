d7738ff024705d5860a6938bc595adf5428d52ba
Ignacio Horcada bernal <ihorcada@c2r4s3.42madrid.com>
2020-12-11 14:28:28 +0100

    norminette OK

--- move_player.c
+++ move_player.c
@@ -6,52 +6,60 @@
 /*   By: ihorcada <ihorcada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 16:17:37 by IgnacioHB         #+#    #+#             */
-/*   Updated: 2020/12/11 14:05:16 by ihorcada         ###   ########.fr       */
+/*   Updated: 2020/12/11 14:27:08 by ihorcada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cubelib.h"
 
-void move_up(t_vars *vars)
+void	move_up(t_vars *vars)
 {
 	if (vars->w == 1)
 	{
-		if (vars->posX + vars->dirX * vars->moveSpeed >= 1 && vars->posX + vars->dirX * vars->moveSpeed < (double)mapWidth - 1)
+		if (vars->posX + vars->dirX * vars->moveSpeed > 1 &&
+			vars->posX + vars->dirX * vars->moveSpeed < (double)mapWidth - 1)
 			vars->posX += vars->dirX * vars->moveSpeed;
-		if (vars->posY + vars->dirY * vars->moveSpeed >= 1 && vars->posY + vars->dirY * vars->moveSpeed < (double)mapHeight - 1)
+		if (vars->posY + vars->dirY * vars->moveSpeed > 1 &&
+			vars->posY + vars->dirY * vars->moveSpeed < (double)mapHeight - 1)
 			vars->posY += vars->dirY * vars->moveSpeed;
 	}
 }
 
-void move_down(t_vars *vars)
+void	move_down(t_vars *vars)
 {
-    if (vars->s == 1) // S
+	if (vars->s == 1)
 	{
-		if (vars->posX - vars->dirX * vars->moveSpeed >= 1 && vars->posX - vars->dirX * vars->moveSpeed < (double)mapWidth - 1)
+		if (vars->posX - vars->dirX * vars->moveSpeed > 1 &&
+			vars->posX - vars->dirX * vars->moveSpeed < (double)mapWidth - 1)
 			vars->posX -= vars->dirX * vars->moveSpeed;
-		if (vars->posY - vars->dirY * vars->moveSpeed >= 1 && vars->posY - vars->dirY * vars->moveSpeed < (double)mapHeight - 1)
+		if (vars->posY - vars->dirY * vars->moveSpeed > 1 &&
+			vars->posY - vars->dirY * vars->moveSpeed < (double)mapHeight - 1)
 			vars->posY -= vars->dirY * vars->moveSpeed;
 	}
 }
 
-void move_left(t_vars *vars)
+void	move_left(t_vars *vars)
 {
-    if (vars->a == 1) // A
+	if (vars->a == 1)
 	{
-		if (vars->posX - vars->dirY * vars->moveSpeed >= 1 && vars->posX - vars->dirY * vars->moveSpeed < (double)mapWidth - 1)
+		if (vars->posX - vars->dirY * vars->moveSpeed > 1 &&
+			vars->posX - vars->dirY * vars->moveSpeed < (double)mapWidth - 1)
 			vars->posX -= vars->dirY * vars->moveSpeed;
-		if (vars->posY + vars->dirX * vars->moveSpeed >= 1 && vars->posY + vars->dirX * vars->moveSpeed < (double)mapHeight - 1)
+		if (vars->posY + vars->dirX * vars->moveSpeed > 1 &&
+			vars->posY + vars->dirX * vars->moveSpeed < (double)mapHeight - 1)
 			vars->posY += vars->dirX * vars->moveSpeed;
 	}
 }
 
-void move_right(t_vars *vars)
+void	move_right(t_vars *vars)
 {
-    if (vars->d == 1) // D
+	if (vars->d == 1)
 	{
-		if (vars->posX + vars->dirY * vars->moveSpeed >= 1 && vars->posX + vars->dirY * vars->moveSpeed < (double)mapWidth - 1)
+		if (vars->posX + vars->dirY * vars->moveSpeed > 1 &&
+			vars->posX + vars->dirY * vars->moveSpeed < (double)mapWidth - 1)
 			vars->posX += vars->dirY * vars->moveSpeed;
-		if (vars->posY - vars->dirX * vars->moveSpeed >= 1 && vars->posY - vars->dirX * vars->moveSpeed < (double)mapHeight - 1)
+		if (vars->posY - vars->dirX * vars->moveSpeed > 1 &&
+			vars->posY - vars->dirX * vars->moveSpeed < (double)mapHeight - 1)
 			vars->posY -= vars->dirX * vars->moveSpeed;
 	}
 }
