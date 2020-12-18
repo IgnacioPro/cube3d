373a9b39baacad7459ed0a496aa9cb96c3cd74f1
Ignacio Horcada bernal <ihorcada@c2r4s3.42madrid.com>
2020-12-18 14:37:38 +0100

    Reading textures and RGB

--- map/read_map.c
+++ map/read_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   read_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: IgnacioHB <IgnacioHB@student.42.fr>        +#+  +:+       +#+        */
+/*   By: ihorcada <ihorcada@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 12:14:47 by ihorcada          #+#    #+#             */
-/*   Updated: 2020/12/17 20:02:50 by IgnacioHB        ###   ########.fr       */
+/*   Updated: 2020/12/18 14:36:24 by ihorcada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,18 @@ typedef struct s_data
 	int x;
 	int y;
 	int i;
+	
+	char *north;
+	char *east;
+	char *west;
+	char *south;
+	char *sprite;
+	int floor[3];
+	int ceiling[3];
+
+	
+	
+	
 } t_data;
 
 void map_name_validator(t_data data)
@@ -31,9 +43,11 @@ void map_name_validator(t_data data)
 	lastFour = ft_substr(data.arg1, (ft_strlen(data.arg1) - 4), 5);
 	j = ft_strncmp(lastFour, ".cub", 4);
 	if (j != 0)
+	{
 		printf("Error. Map name is invalid.\n");
+		exit(0);
+	}
 }
-
 void save_image_validator(t_data data)
 {
 	int j;
@@ -43,7 +57,7 @@ void save_image_validator(t_data data)
 	j = ft_strncmp(data.arg2, "--save", 6);
 	if (j != 0)
 	{
-		printf("Second argument is invalid. Try '--save'.\n");
+		perror("Second argument is invalid. Try '--save'.\n");
 		exit(0);
 	}
 }
@@ -63,6 +77,113 @@ void get_resolution(t_data *data)
 		data->i++;
 }
 
+void north_texture(t_data *data)
+{
+	data->i += 2;
+	
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	data->north = &data->linea[data->i];
+	while(ft_isalpha(data->linea[data->i]) && data->linea[data->i] != ' ')
+		data->i++;
+}
+void south_texture(t_data *data)
+{
+	data->i += 2;
+	
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	data->south = &data->linea[data->i];
+	while(ft_isalpha(data->linea[data->i]) && data->linea[data->i] != ' ')
+		data->i++;
+}
+void east_texture(t_data *data)
+{
+	data->i += 2;
+	
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	data->east = &data->linea[data->i];
+	while(ft_isalpha(data->linea[data->i]) && data->linea[data->i] != ' ')
+		data->i++;
+}
+void west_texture(t_data *data)
+{
+	data->i += 2;
+	
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	data->west = &data->linea[data->i];
+	while(ft_isalpha(data->linea[data->i]) && data->linea[data->i] != ' ')
+		data->i++;
+}
+void sprite_texture(t_data *data)
+{
+	data->i ++;
+	
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	data->sprite = &data->linea[data->i];
+	while(ft_isalpha(data->linea[data->i]) && data->linea[data->i] != ' ')
+		data->i++;
+}
+
+void floor_color(t_data *data)
+{
+		data->i++;
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	data->floor[0] = ft_atoi(&data->linea[data->i]);
+	while (ft_isdigit(data->linea[data->i]))
+		data->i++;
+	while (data->linea[data->i] == ',' || data->linea[data->i] == ' ' )
+		data->i++;
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	data->floor[1] = ft_atoi(&data->linea[data->i]);
+	while (ft_isdigit(data->linea[data->i]))
+		data->i++;
+	while (data->linea[data->i] == ',' || data->linea[data->i] == ' ')
+		data->i++;
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	data->floor[2] = ft_atoi(&data->linea[data->i]);
+		// data->i++;
+	// if ((data->linea[data->i]) != '\0')
+	// {
+	// 	perror("RGB Floor Invalid\n");
+	// 	exit(0);
+	// }
+}
+
+void ceiling_color(t_data *data)
+{
+		data->i++;
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	data->ceiling[0] = ft_atoi(&data->linea[data->i]);
+	while (ft_isdigit(data->linea[data->i]))
+		data->i++;
+	while (data->linea[data->i] == ',' || data->linea[data->i] == ' ' )
+		data->i++;
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	data->ceiling[1] = ft_atoi(&data->linea[data->i]);
+	while (ft_isdigit(data->linea[data->i]))
+		data->i++;
+	while (data->linea[data->i] == ',' || data->linea[data->i] == ' ')
+		data->i++;
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	data->ceiling[2] = ft_atoi(&data->linea[data->i]);
+		// data->i++;
+	// if ((data->linea[data->i]) != '\0')
+	// {
+	// 	perror("RGB Floor Invalid\n");
+	// 	exit(0);
+	// }
+}
+
 int main(int argc, char *argv[])
 {
 	t_data data;
@@ -73,16 +194,21 @@ int main(int argc, char *argv[])
 	data.arg2 = argv[2];
 
 	if ((fd = open(argv[1], O_RDONLY)) == -1)
+	{
 		perror("Error opening map\n");
+		exit(0);
+	}
 	if (!data.arg1)
+	{
 		perror("Error. No map provided\n");
+		exit(0);
+	}
 
 	if (argc > 3 || argc < 2)
 	{
 		perror("Number of arguments is invalid\n");
 		exit(0);
 	}
-
 	map_name_validator(data);
 	if (argc == 3)
 		save_image_validator(data);
@@ -96,23 +222,37 @@ int main(int argc, char *argv[])
 			data.i++;
 		if (data.linea[data.i] == 'R')
 			get_resolution(&data);
-		// if (data.linea[0] == 'N' && data.linea[1] == 'O')
-			// printf("%s\n", data.linea);
-		// if (data.linea[0] == 'S' && data.linea[1] == ' ')
-		// 	printf("%s\n", data.linea);
-		// if (data.linea[0] == 'S' && data.linea[1] == 'O')
-		// 	printf("%s\n", data.linea);
-		// if (data.linea[0] == 'E' && data.linea[1] == 'A')
-		// 	printf("%s\n", data.linea);
-		// if (data.linea[0] == 'W' && data.linea[1] == 'E')
-		// 	printf("%s\n", data.linea);
-		// if (data.linea[0] == 'F')
-		// 	printf("%s\n", data.linea);
-		// if (data.linea[0] == 'C')
-		// printf("%s\n", data.linea);
+		if (data.linea[data.i] == 'N' && data.linea[data.i + 1] == 'O')
+			north_texture(&data);
+		if (data.linea[data.i] == 'S' && data.linea[data.i + 1] == ' ')
+			sprite_texture(&data);
+		if (data.linea[data.i] == 'S' && data.linea[data.i + 1] == 'O')
+			south_texture(&data);
+		if (data.linea[data.i] == 'E' && data.linea[data.i + 1] == 'A')
+			east_texture(&data);
+		if (data.linea[data.i] == 'W' && data.linea[data.i + 1] == 'E')
+			west_texture(&data);
+		if (data.linea[data.i] == 'F')
+			floor_color(&data);
+		if (data.linea[data.i] == 'C')
+			ceiling_color(&data);
+		// if (data.linea[data.i] == '0' || data.linea[data.i] == '1' || data.linea[data.i] == '2')
 	}
 	printf("la x es: %d\n", data.x);
 	printf("la y es: %d\n", data.y);
+	printf("Norte:%s\n", data.north);
+	printf("Sur:%s\n", data.south);
+	printf("Este:%s\n", data.east);
+	printf("Oeste:%s\n", data.west);
+	printf("Sprite:%s\n", data.sprite);
+	printf("Floor 1:%d\n", data.floor[0]);
+	printf("Floor 2:%d\n", data.floor[1]);
+	printf("Floor 3:%d\n", data.floor[2]);
+	printf("Ceiling 1:%d\n", data.ceiling[0]);
+	printf("Ceiling 2:%d\n", data.ceiling[1]);
+	printf("Ceiling 3:%d\n", data.ceiling[2]);
 	
+
+
 	return (0);
 }
