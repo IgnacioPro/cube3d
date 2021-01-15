8fef4f72de531759c11dcc959f16de410a518b04
Ignacio Horcada <IgnacioHB@MacBook-Pro-de-Ignacio.local>
2021-01-15 12:04:14 +0100

    resolution parser added to folder

--- map/read_map.c
+++ map/read_map.c
@@ -6,7 +6,7 @@
 /*   By: IgnacioHB <IgnacioHB@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 12:14:47 by ihorcada          #+#    #+#             */
-/*   Updated: 2021/01/15 11:46:13 by IgnacioHB        ###   ########.fr       */
+/*   Updated: 2021/01/15 12:03:27 by IgnacioHB        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,57 +38,6 @@ void	save_image_validator(t_data data)
 		ft_putstr_fd("Error\nSecond argument is invalid. Try '--save'.\n", 2);
 		exit(0);
 	}
-}
-
-int		valid_resolution(int c)
-{
-	if (ft_isdigit(c) || c == 32)
-		return (1);
-	return (0);
-}
-
-void	resolution_format(t_data *data)
-{
-	int i;
-
-	data->y = -1;
-	i = data->i + 1;
-	while (data->linea[i] != '\0')
-	{
-		if (!(valid_resolution(data->linea[i])))
-			resolution_error();
-		i++;
-	}
-	data->i++;
-}
-
-void	get_resolution(t_data *data)
-{
-	resolution_format(data);
-	if (data->linea[data->i] != ' ')
-		resolution_error();
-	data->i++;
-	while (data->linea[data->i] == ' ')
-		data->i++;
-	if (ft_isdigit(data->linea[data->i]))
-		data->x = ft_atoi(&data->linea[data->i]);
-	while (ft_isdigit(data->linea[data->i]))
-		data->i++;
-	while (data->linea[data->i] == ' ')
-		data->i++;
-	if (ft_isdigit(data->linea[data->i]))
-		data->y = ft_atoi(&data->linea[data->i]);
-	while (ft_isdigit(data->linea[data->i]))
-		data->i++;
-	while (data->linea[data->i] != '\0')
-	{
-		if (data->linea[data->i] != ' ')
-			resolution_error();
-		data->i++;
-	}
-	if (data->y <= 0 || data->x <= 0)
-		resolution_error();
-	data->n_lines++;
 }
 
 int		invalid_map_chars(int c)
---
+++ map/resolution/parse_resolution.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_resolution.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: IgnacioHB <IgnacioHB@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/15 11:58:07 by IgnacioHB         #+#    #+#             */
+/*   Updated: 2021/01/15 12:03:29 by IgnacioHB        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../cubelib.h"
+#include "../../libft/libft.h"
+#include "../../getnextline/get_next_line.h"
+
+int		valid_resolution(int c)
+{
+	if (ft_isdigit(c) || c == 32)
+		return (1);
+	return (0);
+}
+
+void	resolution_format(t_data *data)
+{
+	int i;
+
+	data->y = -1;
+	i = data->i + 1;
+	while (data->linea[i] != '\0')
+	{
+		if (!(valid_resolution(data->linea[i])))
+			resolution_error();
+		i++;
+	}
+	data->i++;
+}
+
+void	get_resolution(t_data *data)
+{
+	resolution_format(data);
+	if (data->linea[data->i] != ' ')
+		resolution_error();
+	data->i++;
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	if (ft_isdigit(data->linea[data->i]))
+		data->x = ft_atoi(&data->linea[data->i]);
+	while (ft_isdigit(data->linea[data->i]))
+		data->i++;
+	while (data->linea[data->i] == ' ')
+		data->i++;
+	if (ft_isdigit(data->linea[data->i]))
+		data->y = ft_atoi(&data->linea[data->i]);
+	while (ft_isdigit(data->linea[data->i]))
+		data->i++;
+	while (data->linea[data->i] != '\0')
+	{
+		if (data->linea[data->i] != ' ')
+			resolution_error();
+		data->i++;
+	}
+	if (data->y <= 0 || data->x <= 0)
+		resolution_error();
+	data->n_lines++;
+}
